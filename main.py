11fda89840f8333fc430f1dcbe89e9f36dca2094
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-01-28 11:41:04 +0900

    update

--- main.py
+++ main.py
@@ -19,4 +19,20 @@ def create_memo(memo:Memo):
 def read_memo():
     return memoes
 
+@app.put('/memoes/{memo_id}')
+def put_memo(req_memo:Memo):
+    for memo in memoes:
+        if memo.id == req_memo.id:
+            memo.content = req_memo.content
+            return '성공했습니다.'
+    return 'not memo'
+
+@app.delete("/memoes/{memo_id}")
+def delete_memo(memo_id:int):
+    for index, memo in enumerate(memoes):
+        if memo.id == memo_id:
+            memoes.pop(index)
+            return '성공했습니다.'
+    return 'not memo', print(memoes)
+
 app.mount("/memo", StaticFiles(directory="static", html=True), name="static")
--- static/app.js
+++ static/app.js
@@ -1,8 +1,45 @@
+async function deleteMemo(event) {
+  const id = event.target.dataset.id;
+  const res = await fetch(`/memoes/${id}`, {
+    method: "DELETE",
+  });
+  readMemo();
+}
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 값을 입력하세요.");
+  const res = await fetch(`/memoes/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id, // === id로만 적어도 js에선 id:id로 됨
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
 function displayMemo(memo) {
   const ul = document.querySelector("#memo-ul");
+
   const li = document.createElement("li");
-  li.innerText = `[id:${memo.id}] ${memo.content}`;
+  li.innerText = `${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "삭제";
+  delBtn.addEventListener("click", deleteMemo);
+  delBtn.dataset.id = memo.id;
+
   ul.appendChild(li);
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
 }
 
 async function readMemo() {
